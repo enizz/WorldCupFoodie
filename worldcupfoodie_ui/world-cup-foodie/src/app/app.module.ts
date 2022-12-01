6755c46eae15cc963530cc3226b82cfb9fc5a844
Eugene <Eugene.Nisnevich@Gmail.com>
2022-11-30 20:19:18 -0500

    autocomplete dropdown implemented

--- worldcupfoodie_ui/world-cup-foodie/src/app/app.module.ts
+++ worldcupfoodie_ui/world-cup-foodie/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PocComponent } from './poc/poc.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DropdownFilterPipe } from './filters/dropdown-filter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     PocComponent,
-    PocComponent
+    PocComponent,
+    DropdownFilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
 
   providers: [],
