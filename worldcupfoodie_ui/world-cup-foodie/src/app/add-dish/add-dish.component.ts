a61e07d571a9ec57f9d0d9b1ec8cd436bd8695c4
Eddy <eduardo.di.garcia@gmail.com>
2022-12-07 00:38:44 -0500

    ngFor on dishes

--- worldcupfoodie_ui/world-cup-foodie/src/app/add-dish/add-dish.component.ts
+++ worldcupfoodie_ui/world-cup-foodie/src/app/add-dish/add-dish.component.ts
@@ -3,19 +3,23 @@ import { DishCrudService } from '../dish-crud.service';
 import { Router, RouterModule, Routes } from '@angular/router';
 import { WorldCupMatchesService } from '../world-cup-matches.service';
 import { WorldCupMatchInfo } from '../world-cup-match-info';
-
+import { WorldCupDish } from '../interface/world-cup-dish';
 @Component({
   selector: 'app-add-dish',
   templateUrl: './add-dish.component.html',
   styleUrls: ['./add-dish.component.css']
 })
 export class AddDishComponent implements OnInit {
+  allDishes: WorldCupDish[] = [];
 
   constructor(private dishcrudstuff: DishCrudService ,private router: Router,private service2:WorldCupMatchesService) { }
   focusedMatch = <WorldCupMatchInfo> {} as WorldCupMatchInfo;
+  focusedDish = <WorldCupDish> {} as WorldCupDish;
 
   ngOnInit(): void {
     this.service2.getFocusedMatch().subscribe((data:WorldCupMatchInfo)=>this.focusedMatch=data);
+    this.dishcrudstuff.getAllDishes().subscribe((data:WorldCupDish[])=>this.allDishes=data);
+    this.dishcrudstuff.getFocusedDish(2).subscribe((data:WorldCupDish)=>this.focusedDish=data);
 
   }
 
--- worldcupfoodie_ui/world-cup-foodie/src/app/dish-crud.service.ts
+++ worldcupfoodie_ui/world-cup-foodie/src/app/dish-crud.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { WorldCupDish } from './interface/world-cup-dish';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishCrudService {
+  dishID=1;
 
-  constructor() { }
+  backendURL:string='https://localhost:7235/api';
+
+  constructor(private httpClient:HttpClient) { }
+  getAllDishes = ():Observable<WorldCupDish[]> => {
+  return this.httpClient.get<WorldCupDish[]>(this.backendURL + "/Dishes");
+}
+
+getFocusedDish= ( incomingDishId: number):Observable<WorldCupDish> => {
+  return this.httpClient.get<WorldCupDish>(this.backendURL + "/Dishes/"+incomingDishId);
+}
+
+eventBeingUsed(idEvent: number){
+   
+  this.dishID = idEvent;
+  
+}
+returnEventID(): number {
+return this.dishID;
+}
 }
