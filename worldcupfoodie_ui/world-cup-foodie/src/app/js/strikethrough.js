3d80af8ed556acb48a7dfacea524740037a9bb69
Gustavo Rivera <garp2100@gmail.com>
2022-12-07 16:35:33 -0500

    working strikethrough functionality

--- worldcupfoodie_ui/world-cup-foodie/src/app/js/strikethrough.js
+++
@@ -1,9 +0,0 @@
-function my_fun(j){
-  var chkbox ="ckb" + j;
-  var my_span ="my_span" + j;
-  var msg = chkbox + "  " + my_span;
-  if(document.getElementById(chkbox).checked){
-  document.getElementById(my_span).style.textDecoration='line-through';
-  }else{
-  document.getElementById(my_span).style.textDecoration='none';
-  }}
